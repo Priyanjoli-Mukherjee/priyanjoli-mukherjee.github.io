5397541050903bbbfd89fade9d7cdbfbb37a7186
Priya Mukherjee <priyanjolim@gmail.com>
2024-01-26 17:22:38 -0800

    moved data into own call

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,25 @@
 import { Box, Paper } from "@mui/material";
+import { useMemo } from "react";
 import "./app.css";
+import { getTweets } from "./get-tweets";
 
 export function App() {
- const arr = [{name: "Ethel", twitterHandle: "@oldtimerethel", time: 56, message: "What's going on?"}, {name: "JimBob", twitterHandle:"@jimbobWW2", time: 2, message: "What to do?"}, {name: "Gertrude", twitterHandle: "Gertrude1920", time: 48, message: "Is the war over?"}];
- 
- return (
+  const tweets = useMemo(() => getTweets(), []);
+
+  return (
     <>
-    <Box>
-      Home
-    </Box>
-    <Box>
-      What's Happening?
-    </Box>
-      {arr.map((ele) => (
-        <Box width={665}>
-        <Paper elevation={5} style={{height: 100}}>
-          {ele.name}
-        </Paper>
-        <Paper elevation={5}>
-          {ele.twitterHandle}
-        </Paper>
-        <Paper elevation={5}>
-          {ele.time}
-        </Paper>
-        <Paper elevation={5}>
-          {ele.message}
-        </Paper>
-      </Box>
+      <Box>Home</Box>
+      <Box>What's Happening?</Box>
+      {tweets.map((ele) => (
+        <Box key={ele.id} width={665}>
+          <Paper elevation={5} style={{ height: 100 }}>
+            {ele.name}
+          </Paper>
+          <Paper elevation={5}>{ele.twitterHandle}</Paper>
+          <Paper elevation={5}>{ele.time}</Paper>
+          <Paper elevation={5}>{ele.message}</Paper>
+        </Box>
       ))}
     </>
   );
 }
-
---
+++ src/get-tweets.ts
@@ -0,0 +1,25 @@
+export function getTweets() {
+  return [
+    {
+      id: "tweet0",
+      name: "Ethel",
+      twitterHandle: "@oldtimerethel",
+      time: 56,
+      message: "What's going on?",
+    },
+    {
+      id: "tweet1",
+      name: "JimBob",
+      twitterHandle: "@jimbobWW2",
+      time: 2,
+      message: "What to do?",
+    },
+    {
+      id: "tweet2",
+      name: "Gertrude",
+      twitterHandle: "Gertrude1920",
+      time: 48,
+      message: "Is the war over?",
+    },
+  ];
+}

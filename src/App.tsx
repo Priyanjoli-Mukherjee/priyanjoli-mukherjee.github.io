f99b298dba2abd3eb55fb8293d7adba4e07f50ee
Priya Mukherjee <priyanjolim@gmail.com>
2024-01-26 17:10:14 -0800

    added array of cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Box, Paper } from "@mui/material";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
+export function App() {
+ const arr = [{name: "Ethel", twitterHandle: "@oldtimerethel", time: 56, message: "What's going on?"}, {name: "JimBob", twitterHandle:"@jimbobWW2", time: 2, message: "What to do?"}, {name: "Gertrude", twitterHandle: "Gertrude1920", time: 48, message: "Is the war over?"}];
+ 
+ return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Box>
+      Home
+    </Box>
+    <Box>
+      What's Happening?
+    </Box>
+      {arr.map((ele) => (
+        <Box width={665}>
+        <Paper elevation={5} style={{height: 100}}>
+          {ele.name}
+        </Paper>
+        <Paper elevation={5}>
+          {ele.twitterHandle}
+        </Paper>
+        <Paper elevation={5}>
+          {ele.time}
+        </Paper>
+        <Paper elevation={5}>
+          {ele.message}
+        </Paper>
+      </Box>
+      ))}
     </>
   );
 }
 
-export default App;

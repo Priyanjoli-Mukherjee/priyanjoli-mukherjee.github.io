9114912b0acfc97a5ac9108588bfd3c919c0af68
Priya Mukherjee <priyanjolim@gmail.com>
2024-02-14 13:25:43 -0800

    prettier

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ export function App() {
         paddingBottom={2}
         style={{
           backgroundColor: "white",
-          borderBottom: "1px solid lightgrey"
+          borderBottom: "1px solid lightgrey",
         }}
       >
         <Typography variant="h6" sx={{ fontWeight: 900, color: "black" }}>
@@ -31,29 +31,34 @@ export function App() {
         height={70}
         marginLeft={1}
         marginRight={1}
-        style={{ backgroundColor: "white", borderBottom: "10px solid lightgrey" }}
+        style={{
+          backgroundColor: "white",
+          borderBottom: "10px solid lightgrey",
+        }}
       >
         <Typography variant="h6" sx={{ fontWeight: 500, color: "lightgrey" }}>
-        What's Happening?
+          What's Happening?
         </Typography>
       </Box>
       {tweets.map((ele) => (
-        <Box key={ele.id} marginLeft={1} marginRight={1} marginBottom={1} style={{ backgroundColor: "white" }}>
+        <Box
+          key={ele.id}
+          marginLeft={1}
+          marginRight={1}
+          marginBottom={1}
+          style={{ backgroundColor: "white" }}
+        >
           <Box display="flex" alignItems="center" paddingLeft={1}>
             <Box color="black">
               <Typography variant="body1" sx={{ fontWeight: 900 }}>
-              {ele.name}
-              </Typography>
-              </Box>
-            <Box color="black" paddingLeft={2}>
-              <Typography variant="body1">
-              {ele.twitterHandle}
+                {ele.name}
               </Typography>
             </Box>
             <Box color="black" paddingLeft={2}>
-              <Typography variant="body1">
-              {formatDate(ele.time)}
-              </Typography>
+              <Typography variant="body1">{ele.twitterHandle}</Typography>
+            </Box>
+            <Box color="black" paddingLeft={2}>
+              <Typography variant="body1">{formatDate(ele.time)}</Typography>
             </Box>
           </Box>
           <Box
@@ -65,9 +70,7 @@ export function App() {
             justifyContent="flex-start"
             alignItems="center"
           >
-            <Typography variant="body2">
-            {ele.message}
-            </Typography>
+            <Typography variant="body2">{ele.message}</Typography>
           </Box>
         </Box>
       ))}

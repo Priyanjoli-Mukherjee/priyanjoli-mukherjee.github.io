074a6d53c0eb62b8095c1a91748d968bf483bdc4
Priya Mukherjee <priyanjolim@gmail.com>
2024-03-06 18:17:09 -0800

    ordered tweets and messages by time

--- src/service/initialize-tweets.ts
+++ src/service/initialize-tweets.ts
@@ -6,10 +6,12 @@ import { randomUser } from "../random-utils/random-user";
 import { Tweet } from "../types/tweet";
 
 export function initializeTweets(): Array<Tweet> {
-  return range(15).map(() => ({
-    id: uniqueId("tweet"),
-    ...randomUser(),
-    time: randomInteger(Date.now()),
-    message: randomText(),
-  }));
+  return range(15)
+    .map(() => ({
+      id: uniqueId("tweet"),
+      ...randomUser(),
+      time: randomInteger(Date.now()),
+      message: randomText(),
+    }))
+    .sort((tweet1, tweet2) => tweet2.time - tweet1.time);
 }
--- src/service/random-messages.ts
+++ src/service/random-messages.ts
@@ -4,17 +4,19 @@ import { randomText } from "../random-utils/random-text";
 import { Message } from "../types/message";
 import { User } from "../types/user";
 import { getCurrentUser } from "./get-current-user";
+import { randomInteger } from "../random-utils/random-integer";
 
 export function randomMessages(
   user1: User,
   user2 = getCurrentUser(),
 ): Array<Message> {
-  const now = Date.now();
-  return range(10).map((index) => ({
-    id: uniqueId("message"),
-    message: randomText(),
-    twitterHandle:
-      Math.random() < 0.5 ? user1.twitterHandle : user2.twitterHandle,
-    time: (index * now) / 10,
-  }));
+  return range(10)
+    .map(() => ({
+      id: uniqueId("message"),
+      message: randomText(),
+      twitterHandle:
+        Math.random() < 0.5 ? user1.twitterHandle : user2.twitterHandle,
+      time: randomInteger(Date.now()),
+    }))
+    .sort((message1, message2) => message1.time - message2.time);
 }

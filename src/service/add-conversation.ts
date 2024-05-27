8a8fc15c544c3c656ab906228b79ecfc421f5aae
Priya Mukherjee <priyanjolim@gmail.com>
2024-05-27 11:57:26 -0700

    added endpoint for adding conversation

---
+++ src/service/add-conversation.ts
@@ -0,0 +1,9 @@
+import { USERS } from "../random-utils/random-user";
+import { conversations } from "./conversations";
+
+export function addConversation(twitterHandle: string) {
+    const user = USERS.find((u) => u.twitterHandle === twitterHandle);
+    if (user) [
+        conversations.push({messages: [], user})
+    ]
+}

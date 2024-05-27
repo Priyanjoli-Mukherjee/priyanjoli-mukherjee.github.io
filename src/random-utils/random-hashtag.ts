667803276e0bff305dae11b4bf379a1482fb5c88
Priya Mukherjee <priyanjolim@gmail.com>
2024-05-27 11:18:43 -0700

    changed hashtags

--- src/random-utils/random-hashtag.ts
+++ src/random-utils/random-hashtag.ts
@@ -1,15 +1,15 @@
 import { randomInteger } from "./random-integer";
 
 export const HASHTAGS = [
-  ...new Array(5).fill("#DonaldTrump"),
+  ...new Array(5).fill("#MondayMotivation"),
   ...new Array(4).fill("#LisanAlGaib"),
-  ...new Array(7).fill("#KateMiddletonIsDead"),
+  ...new Array(7).fill("#WeekendVibes"),
   "#Girls5Eva",
-  ...new Array(4).fill("#DarkBrandon"),
+  ...new Array(4).fill("#TechNews"),
   ...new Array(9).fill("#Election2024"),
-  ...new Array(4).fill("#OscarsSoWhite"),
-  ...new Array(6).fill("#BabyJesus"),
-  ...new Array(20).fill("#MidgetsForTrump"),
+  ...new Array(4).fill("#FitnessMotivation"),
+  ...new Array(6).fill("#Oscars2024"),
+  ...new Array(20).fill("#AI"),
   ...new Array(4).fill("#WheresKate"),
 ];
 

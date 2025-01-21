369fe0fb7d1b90886a7362eb027847518bd3f056
Priya Mukherjee <priyanjolim@gmail.com>
2025-01-21 12:59:39 -0800

    disabled button

--- src/tweet-page.tsx
+++ src/tweet-page.tsx
@@ -38,7 +38,7 @@ export function TweetPage() {
         >
           <textarea
             rows={2}
-            placeholder="Tweet your reply"
+            placeholder="Post your reply"
             value={replyMessage}
             style={{
               backgroundColor: "white",
@@ -63,6 +63,7 @@ export function TweetPage() {
           >
             <Button
               variant="contained"
+              disabled={!replyMessage}
               sx={{ height: 35, borderRadius: 4 }}
               onClick={() => {
                 addTweet(replyMessage, tweetId);

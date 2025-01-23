bcb6f8ea82ea249a6cffd89fd15ce350ef12f226
Priya Mukherjee <priyanjolim@gmail.com>
2025-01-22 17:16:36 -0800

    fixed logic in randomText

--- src/random-utils/random-text.ts
+++ src/random-utils/random-text.ts
@@ -2,7 +2,16 @@ import { LOREM_IPSUM } from "./lorem-ipsum";
 import { randomInteger } from "./random-integer";
 
 export function randomText() {
-  const i = randomInteger(LOREM_IPSUM.length);
-  const j = randomInteger(LOREM_IPSUM.length);
-  return LOREM_IPSUM.slice(Math.min(i, j), Math.max(i, j));
+  const regex = /[A-Z]/g;
+  const regexPeriod = /\./g;
+
+  const capitalLetterIndices = LOREM_IPSUM.split("").map((letter, index) => letter.match(regex) ? index : null).filter(( index ) => index !== null);
+  
+  const i = capitalLetterIndices[randomInteger(capitalLetterIndices.length)];
+  
+  const periodIndices = LOREM_IPSUM.slice(i + 1).split("").map((letter, index) => letter.match(regexPeriod) ? index : null).filter(( index ) => index !== null);
+
+  const j = periodIndices[randomInteger(periodIndices.length)] + i + 1;
+
+  return LOREM_IPSUM.slice(i, j + 1);
 }

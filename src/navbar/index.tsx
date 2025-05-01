3b701e71eece4e0f5f72afc9c2a03ea1af12ebdc
Priyanjoli Mukherjee <priyanjolim@gmail.com>
2025-04-30 23:37:23 -0700

    fix: fixed styling of navbar

--- src/navbar/index.tsx
+++ src/navbar/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Accordion,
   AccordionDetails,
@@ -10,9 +11,14 @@ import MailIcon from "@mui/icons-material/Mail";
 import WorkIcon from "@mui/icons-material/Work";
 import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
 import { NavAccordionSummary } from "./nav-accordion-summary";
-import { Home } from "@mui/icons-material";
+import Home from "@mui/icons-material/Home";
+import MenuIcon from "@mui/icons-material/Menu";
+import PeopleIcon from "@mui/icons-material/People";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 
 export function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <Paper
       elevation={8}
@@ -27,51 +33,87 @@ export function Navbar() {
         color: "white",
       }}
     >
+      <Box
+        onClick={() => setMenuOpen(!isMenuOpen)}
+        margin={1}
+        color="white"
+        sx={{ cursor: "pointer" }}
+      >
+        {isMenuOpen ? (
+          <Box display="flex" alignItems="center">
+            <MenuOpenIcon />
+            <Typography
+              component="em"
+              variant="body1"
+              sx={{ paddingLeft: 0.5 }}
+            >
+              Collapse
+            </Typography>
+          </Box>
+        ) : (
+          <MenuIcon fontSize="small" />
+        )}
+      </Box>
       <Box margin={1}>
         <Link to="/">
           <Box alignItems="center" display="flex">
             <Home fontSize="small" />
-            <Typography variant="body1" sx={{ paddingLeft: 1 }}>
-              Home
-            </Typography>
+            {isMenuOpen && (
+              <Typography variant="body1" sx={{ paddingLeft: 1 }}>
+                Home
+              </Typography>
+            )}
           </Box>
         </Link>
       </Box>
       <Box margin={1}>
-        <Accordion
-          defaultExpanded
-          sx={{
-            backgroundColor: "unset",
-            boxShadow: "unset",
-            color: "white",
-            padding: 0,
-          }}
-        >
-          <NavAccordionSummary
-            expandIcon={
-              <ArrowForwardIosSharpIcon
-                sx={{ color: "white", fontSize: "0.9rem" }}
-              />
-            }
+        {isMenuOpen ? (
+          <Accordion
+            defaultExpanded
+            sx={{
+              backgroundColor: "unset",
+              boxShadow: "unset",
+              color: "white",
+              padding: 0,
+            }}
           >
-            <Typography variant="body1" sx={{ marginLeft: 1 }}>
-              Projects
-            </Typography>
-          </NavAccordionSummary>
-          <AccordionDetails sx={{ paddingBottom: 0, paddingLeft: 3 }}>
-            <Link to="/scrollr">
-              <Typography variant="body1">Scrollr</Typography>
-            </Link>
-          </AccordionDetails>
-        </Accordion>
+            <NavAccordionSummary
+              expandIcon={
+                <ArrowForwardIosSharpIcon
+                  sx={{ color: "white", fontSize: "0.9rem" }}
+                />
+              }
+            >
+              <Typography variant="body1" sx={{ marginLeft: 1 }}>
+                Projects
+              </Typography>
+            </NavAccordionSummary>
+            <AccordionDetails sx={{ paddingBottom: 0, paddingLeft: 3 }}>
+              <Link to="/scrollr">
+                <Box alignItems="center" display="flex">
+                  <PeopleIcon fontSize="small" />
+                  <Typography variant="body1" sx={{ paddingLeft: 1 }}>
+                    Scrollr
+                  </Typography>
+                </Box>
+              </Link>
+            </AccordionDetails>
+          </Accordion>
+        ) : (
+          <Link to="/scrollr">
+            <PeopleIcon fontSize="small" />
+          </Link>
+        )}
       </Box>
       <Box margin={1}>
         <Link to="/contact">
           <Box alignItems="center" display="flex">
             <MailIcon fontSize="small" />
-            <Typography variant="body1" sx={{ paddingLeft: 1 }}>
-              Contact
-            </Typography>
+            {isMenuOpen && (
+              <Typography variant="body1" sx={{ paddingLeft: 1 }}>
+                Contact
+              </Typography>
+            )}
           </Box>
         </Link>
       </Box>
@@ -79,9 +121,11 @@ export function Navbar() {
         <Link to="/resume">
           <Box alignItems="center" display="flex">
             <WorkIcon fontSize="small" />
-            <Typography variant="body1" sx={{ paddingLeft: 1 }}>
-              Resume
-            </Typography>
+            {isMenuOpen && (
+              <Typography variant="body1" sx={{ paddingLeft: 1 }}>
+                Resume
+              </Typography>
+            )}
           </Box>
         </Link>
       </Box>

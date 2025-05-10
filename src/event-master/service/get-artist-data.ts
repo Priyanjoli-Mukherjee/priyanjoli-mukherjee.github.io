ff25db0b14a45aeb0d093703d64cd99eee08db1d
Priyanjoli Mukherjee <priyanjolim@gmail.com>
2025-05-10 16:18:24 -0700

    refactor: hardcoded ids for artists

--- src/event-master/service/get-artist-data.ts
+++ src/event-master/service/get-artist-data.ts
@@ -1,4 +1,3 @@
-import uniqueId from "lodash/uniqueId";
 import { Artist } from "../types/artist";
 import TaylorSwift from "../../images/event-master/Taylor_Swift.jpg";
 import Philharmonic from "../../images/event-master/Philharmonic.jpg";
@@ -15,82 +14,102 @@ import Polynesian from "../../images/event-master/Polynesian.jpg";
 
 export const artistData: Artist[] = [
   {
+    description: "",
     genre: "Pop",
+    id: "taylor-swift",
     image: TaylorSwift,
     imageOffset: 15,
     name: "Taylor Swift",
   },
   {
+    description: "",
     genre: "Classical",
+    id: "dublin-philharmonic",
     image: Philharmonic,
     imageOffset: 30,
     name: "Dublin Philharmonic",
   },
   {
+    description: "",
     genre: "Heavy Metal",
+    id: "metallica",
     image: Metallica,
     imageOffset: 5,
     name: "Metallica",
   },
   {
+    description: "",
     genre: "Electronic",
+    id: "electric-boogaloo",
     image: DJ,
     imageOffset: 20,
     name: "Electric Boogaloo",
   },
   {
+    description: "",
     genre: "Film Scores",
+    id: "magic-music-days",
     image: Disney,
     imageOffset: 15,
     name: "Magic Music Days",
   },
   {
+    description: "",
     genre: "Electronic",
+    id: "bob-moses",
     image: GriffithObservatory,
     imageOffset: 25,
     name: "Bob Moses",
   },
   {
+    description: "",
     genre: "Latin",
+    id: "mariachi-huenachi",
     image: MariachiHuenachi,
     imageOffset: 10,
     name: "Mariachi Huenachi",
   },
   {
+    description: "",
     genre: "Punjabi",
+    id: "bhangra-boys",
     image: Bhangra,
     imageOffset: 15,
     name: "Bhangra Boys",
   },
   {
+    description: "",
     genre: "Latin",
+    id: "flamenco-andalusia",
     image: Flamenco,
     imageOffset: 10,
     name: "Flamenco Andalusia",
   },
   {
+    description: "",
     genre: "Classical",
+    id: "pops-in-the-park",
     image: Fireworks,
     imageOffset: 10,
     name: "Pops in the Park",
   },
   {
+    description: "",
     genre: "Country",
+    id: "garth-brooks",
     image: Rodeo,
     imageOffset: 10,
     name: "Garth Brooks",
   },
   {
+    description: "",
     genre: "Polynesian",
+    id: "hula-gals",
     image: Polynesian,
     imageOffset: 10,
     name: "The Hula Gals",
   },
-].map((artist) => ({
-  ...artist,
-  description: "",
-  id: uniqueId("artist"),
-}));
+];
 
 export function getArtistData() {
   return artistData;

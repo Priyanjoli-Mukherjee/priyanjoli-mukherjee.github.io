cae74acf727b4a2ecf813c059f69ce43ce5986b5
Priyanjoli Mukherjee <priyanjolim@gmail.com>
2025-07-06 17:24:58 -0700

    feat: added tasks endpoint

--- src/kanban/kanban-page.tsx
+++ src/kanban/kanban-page.tsx
@@ -1,5 +1,13 @@
 import { Box } from "@mui/material";
+import { useTasks } from "../hooks/use-tasks";
 
 export function Kanban() {
-  return <Box></Box>;
+  const tasks = useTasks();
+  return (
+    <Box>
+      {tasks.map((task) => (
+        <Box key={task.id}>{task.title}</Box>
+      ))}
+    </Box>
+  );
 }
